621bf40bc138131cb8ffacfa8320aa18e914b186
abdurrehman96x <abdurrehman96x@gmail.com>
2025-09-02 21:24:03 +0530

    frontend deployment

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://blog-nest-frontend-five.vercel.app"],
+    origin: true,
     credentials: true,
   })
 );
