59ad6accae5dd1428cfdb7f9067020d959df6965
abdurrehman96x <abdurrehman96x@gmail.com>
2025-10-08 17:13:40 +0530

    added 5mb limit upload

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,4 +1,8 @@
-import multer from "multer"
+import multer from "multer";
 
-const storage = multer.memoryStorage();
-export const singleUpload = multer({storage}).single("file");
+const storage = multer.memoryStorage(); 
+
+export const singleUpload = multer({
+    storage,
+    limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+}).single("file");
